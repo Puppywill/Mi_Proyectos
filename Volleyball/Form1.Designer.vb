7f36dc5fbc7937a60dfe5854d016a951fb05985a
William Rosado <109486989+Puppywill@users.noreply.github.com>
2023-10-17 18:37:58 -0400

    Volleyball
    Volleyball sell 
    Visual basic

---
+++ Volleyball/Form1.Designer.vb
@@ -0,0 +1,401 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Gropodeventa = New System.Windows.Forms.GroupBox()
+        Me.Txtnivel3 = New System.Windows.Forms.TextBox()
+        Me.Txtnivel2 = New System.Windows.Forms.TextBox()
+        Me.Txtnivel1 = New System.Windows.Forms.TextBox()
+        Me.Nivel3 = New System.Windows.Forms.Label()
+        Me.Nivel2 = New System.Windows.Forms.Label()
+        Me.Nivel1 = New System.Windows.Forms.Label()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtResultado = New System.Windows.Forms.TextBox()
+        Me.Resultadogroup = New System.Windows.Forms.GroupBox()
+        Me.BtnResultado = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Lblnivel310 = New System.Windows.Forms.Label()
+        Me.LblNivel3 = New System.Windows.Forms.Label()
+        Me.Lblnivel215 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Lblnivel120 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Btnexit = New System.Windows.Forms.Button()
+        Me.Groupventa = New System.Windows.Forms.GroupBox()
+        Me.Textnivel3 = New System.Windows.Forms.TextBox()
+        Me.Textnivel2 = New System.Windows.Forms.TextBox()
+        Me.Txtventa1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Gropodeventa.SuspendLayout()
+        Me.Resultadogroup.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.Groupventa.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.Control
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Image = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Label1.Location = New System.Drawing.Point(210, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(503, 73)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Volleyball Team"
+        '
+        'Gropodeventa
+        '
+        Me.Gropodeventa.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Gropodeventa.Controls.Add(Me.Txtnivel3)
+        Me.Gropodeventa.Controls.Add(Me.Txtnivel2)
+        Me.Gropodeventa.Controls.Add(Me.Txtnivel1)
+        Me.Gropodeventa.Controls.Add(Me.Nivel3)
+        Me.Gropodeventa.Controls.Add(Me.Nivel2)
+        Me.Gropodeventa.Controls.Add(Me.Nivel1)
+        Me.Gropodeventa.Location = New System.Drawing.Point(5, 181)
+        Me.Gropodeventa.Name = "Gropodeventa"
+        Me.Gropodeventa.Size = New System.Drawing.Size(179, 142)
+        Me.Gropodeventa.TabIndex = 1
+        Me.Gropodeventa.TabStop = False
+        Me.Gropodeventa.Text = "Datos"
+        '
+        'Txtnivel3
+        '
+        Me.Txtnivel3.Location = New System.Drawing.Point(64, 113)
+        Me.Txtnivel3.Name = "Txtnivel3"
+        Me.Txtnivel3.Size = New System.Drawing.Size(135, 20)
+        Me.Txtnivel3.TabIndex = 5
+        '
+        'Txtnivel2
+        '
+        Me.Txtnivel2.Location = New System.Drawing.Point(64, 72)
+        Me.Txtnivel2.Name = "Txtnivel2"
+        Me.Txtnivel2.Size = New System.Drawing.Size(135, 20)
+        Me.Txtnivel2.TabIndex = 4
+        '
+        'Txtnivel1
+        '
+        Me.Txtnivel1.Location = New System.Drawing.Point(64, 34)
+        Me.Txtnivel1.Name = "Txtnivel1"
+        Me.Txtnivel1.Size = New System.Drawing.Size(135, 20)
+        Me.Txtnivel1.TabIndex = 3
+        '
+        'Nivel3
+        '
+        Me.Nivel3.AutoSize = True
+        Me.Nivel3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Nivel3.Location = New System.Drawing.Point(10, 113)
+        Me.Nivel3.Name = "Nivel3"
+        Me.Nivel3.Size = New System.Drawing.Size(48, 16)
+        Me.Nivel3.TabIndex = 2
+        Me.Nivel3.Text = "Nivel 3"
+        '
+        'Nivel2
+        '
+        Me.Nivel2.AutoSize = True
+        Me.Nivel2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Nivel2.Location = New System.Drawing.Point(9, 72)
+        Me.Nivel2.Name = "Nivel2"
+        Me.Nivel2.Size = New System.Drawing.Size(48, 16)
+        Me.Nivel2.TabIndex = 1
+        Me.Nivel2.Text = "Nivel 2"
+        '
+        'Nivel1
+        '
+        Me.Nivel1.AutoSize = True
+        Me.Nivel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Nivel1.Location = New System.Drawing.Point(9, 35)
+        Me.Nivel1.Name = "Nivel1"
+        Me.Nivel1.Size = New System.Drawing.Size(48, 16)
+        Me.Nivel1.TabIndex = 0
+        Me.Nivel1.Text = "Nivel 1"
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.ButtonClear.Location = New System.Drawing.Point(336, 390)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(89, 31)
+        Me.ButtonClear.TabIndex = 2
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Image = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Label2.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Assertive
+        Me.Label2.Location = New System.Drawing.Point(52, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Total "
+        '
+        'TxtResultado
+        '
+        Me.TxtResultado.Location = New System.Drawing.Point(26, 79)
+        Me.TxtResultado.Name = "TxtResultado"
+        Me.TxtResultado.Size = New System.Drawing.Size(130, 20)
+        Me.TxtResultado.TabIndex = 4
+        '
+        'Resultadogroup
+        '
+        Me.Resultadogroup.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Resultadogroup.Controls.Add(Me.TxtResultado)
+        Me.Resultadogroup.Controls.Add(Me.Label2)
+        Me.Resultadogroup.Location = New System.Drawing.Point(577, 126)
+        Me.Resultadogroup.Name = "Resultadogroup"
+        Me.Resultadogroup.Size = New System.Drawing.Size(182, 143)
+        Me.Resultadogroup.TabIndex = 3
+        Me.Resultadogroup.TabStop = False
+        Me.Resultadogroup.Text = "Total"
+        '
+        'BtnResultado
+        '
+        Me.BtnResultado.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.BtnResultado.Location = New System.Drawing.Point(151, 390)
+        Me.BtnResultado.Name = "BtnResultado"
+        Me.BtnResultado.Size = New System.Drawing.Size(103, 31)
+        Me.BtnResultado.TabIndex = 4
+        Me.BtnResultado.Text = "Resultado "
+        Me.BtnResultado.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.GroupBox1.Controls.Add(Me.Lblnivel310)
+        Me.GroupBox1.Controls.Add(Me.LblNivel3)
+        Me.GroupBox1.Controls.Add(Me.Lblnivel215)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Lblnivel120)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(5, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(179, 136)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Taquillas"
+        '
+        'Lblnivel310
+        '
+        Me.Lblnivel310.AutoSize = True
+        Me.Lblnivel310.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblnivel310.Location = New System.Drawing.Point(67, 99)
+        Me.Lblnivel310.Name = "Lblnivel310"
+        Me.Lblnivel310.Size = New System.Drawing.Size(70, 24)
+        Me.Lblnivel310.TabIndex = 9
+        Me.Lblnivel310.Text = "$10.00 "
+        '
+        'LblNivel3
+        '
+        Me.LblNivel3.AutoSize = True
+        Me.LblNivel3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblNivel3.Location = New System.Drawing.Point(6, 99)
+        Me.LblNivel3.Name = "LblNivel3"
+        Me.LblNivel3.Size = New System.Drawing.Size(51, 16)
+        Me.LblNivel3.TabIndex = 8
+        Me.LblNivel3.Text = "Nivel 3:"
+        '
+        'Lblnivel215
+        '
+        Me.Lblnivel215.AutoSize = True
+        Me.Lblnivel215.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblnivel215.Location = New System.Drawing.Point(67, 56)
+        Me.Lblnivel215.Name = "Lblnivel215"
+        Me.Lblnivel215.Size = New System.Drawing.Size(70, 24)
+        Me.Lblnivel215.TabIndex = 8
+        Me.Lblnivel215.Text = "$15.00 "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(6, 56)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Nivel 2:"
+        '
+        'Lblnivel120
+        '
+        Me.Lblnivel120.AutoSize = True
+        Me.Lblnivel120.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblnivel120.Location = New System.Drawing.Point(67, 16)
+        Me.Lblnivel120.Name = "Lblnivel120"
+        Me.Lblnivel120.Size = New System.Drawing.Size(70, 24)
+        Me.Lblnivel120.TabIndex = 6
+        Me.Lblnivel120.Text = "$20.00 "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nivel 1:"
+        '
+        'Btnexit
+        '
+        Me.Btnexit.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Btnexit.Location = New System.Drawing.Point(525, 390)
+        Me.Btnexit.Name = "Btnexit"
+        Me.Btnexit.Size = New System.Drawing.Size(94, 31)
+        Me.Btnexit.TabIndex = 6
+        Me.Btnexit.Text = "Exit"
+        Me.Btnexit.UseVisualStyleBackColor = True
+        '
+        'Groupventa
+        '
+        Me.Groupventa.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.Groupventa.Controls.Add(Me.Textnivel3)
+        Me.Groupventa.Controls.Add(Me.Textnivel2)
+        Me.Groupventa.Controls.Add(Me.Txtventa1)
+        Me.Groupventa.Controls.Add(Me.Label7)
+        Me.Groupventa.Controls.Add(Me.Label6)
+        Me.Groupventa.Controls.Add(Me.Label5)
+        Me.Groupventa.Location = New System.Drawing.Point(273, 130)
+        Me.Groupventa.Name = "Groupventa"
+        Me.Groupventa.Size = New System.Drawing.Size(194, 143)
+        Me.Groupventa.TabIndex = 7
+        Me.Groupventa.TabStop = False
+        Me.Groupventa.Text = "Venta"
+        '
+        'Textnivel3
+        '
+        Me.Textnivel3.Location = New System.Drawing.Point(52, 103)
+        Me.Textnivel3.Name = "Textnivel3"
+        Me.Textnivel3.Size = New System.Drawing.Size(134, 20)
+        Me.Textnivel3.TabIndex = 6
+        '
+        'Textnivel2
+        '
+        Me.Textnivel2.Location = New System.Drawing.Point(52, 67)
+        Me.Textnivel2.Name = "Textnivel2"
+        Me.Textnivel2.Size = New System.Drawing.Size(134, 20)
+        Me.Textnivel2.TabIndex = 5
+        '
+        'Txtventa1
+        '
+        Me.Txtventa1.Location = New System.Drawing.Point(52, 32)
+        Me.Txtventa1.Name = "Txtventa1"
+        Me.Txtventa1.Size = New System.Drawing.Size(134, 20)
+        Me.Txtventa1.TabIndex = 4
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(0, 103)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(48, 16)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Nivel 3"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(0, 68)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(48, 16)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Nivel 2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(0, 33)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 16)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Nivel 1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Volleyball.My.Resources.Resources.Volleyball
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Groupventa)
+        Me.Controls.Add(Me.Btnexit)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.BtnResultado)
+        Me.Controls.Add(Me.Resultadogroup)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.Gropodeventa)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Gropodeventa.ResumeLayout(False)
+        Me.Gropodeventa.PerformLayout()
+        Me.Resultadogroup.ResumeLayout(False)
+        Me.Resultadogroup.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.Groupventa.ResumeLayout(False)
+        Me.Groupventa.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Gropodeventa As GroupBox
+    Friend WithEvents Nivel3 As Label
+    Friend WithEvents Nivel2 As Label
+    Friend WithEvents Nivel1 As Label
+    Friend WithEvents Txtnivel3 As TextBox
+    Friend WithEvents Txtnivel2 As TextBox
+    Friend WithEvents Txtnivel1 As TextBox
+    Friend WithEvents ButtonClear As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtResultado As TextBox
+    Friend WithEvents Resultadogroup As GroupBox
+    Friend WithEvents BtnResultado As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Lblnivel215 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Lblnivel120 As Label
+    Friend WithEvents Lblnivel310 As Label
+    Friend WithEvents LblNivel3 As Label
+    Friend WithEvents Btnexit As Button
+    Friend WithEvents Groupventa As GroupBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Textnivel3 As TextBox
+    Friend WithEvents Textnivel2 As TextBox
+    Friend WithEvents Txtventa1 As TextBox
+End Class
---
+++ Volleyball/Form1.vb
@@ -0,0 +1,111 @@
+﻿'Porgrama de venta volleyball'
+'William A.Rosado'
+'Y00599440'
+'Comp-3900'
+
+Public Class Form1
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        Try
+            Dim Resultado As Integer
+
+
+            Txtnivel1.Clear()
+            Txtnivel2.Clear()
+            Txtnivel3.Clear()
+            Txtventa1.Clear()
+            Textnivel2.Clear()
+            Textnivel3.Clear()
+
+
+            TxtResultado.Text = "" ' Limpiar el resultado en la etiqueta'
+            TxtResultado.Text = Resultado.ToString()
+
+        Catch
+            MessageBox.Show("No tiene nada")
+
+        End Try
+    End Sub
+
+    Private Sub BtnResultado_Click(sender As Object, e As EventArgs) Handles BtnResultado.Click
+        Try
+            Const cmulti As Integer = 20
+            Const cmulti2 As Integer = 15
+            Const cmulti3 As Integer = 10
+
+            Dim xResulatdo As Integer
+            xResulatdo = CInt(Txtnivel1.Text * cmulti) + CInt(Txtnivel2.Text * cmulti2) + CInt(Txtnivel3.Text * cmulti3)
+
+
+            TxtResultado.Text = xResulatdo.ToString("c2")
+        Catch
+            MessageBox.Show("Por favor ingresa la taquilla ")
+
+        End Try
+
+        Try
+            Dim Result1 As Integer
+            Result1 = CInt(Txtnivel1.Text * Lblnivel120.Text)
+
+
+
+            Txtventa1.Text = Result1.ToString()
+
+        Catch
+
+
+
+        End Try
+
+        Try
+            Dim Result2 As Integer
+            Result2 = CInt(Txtnivel2.Text * Lblnivel215.Text)
+
+
+
+            Textnivel2.Text = Result2.ToString()
+
+        Catch
+
+
+
+        End Try
+
+
+
+        Try
+            Dim Result3 As Integer
+            Result3 = CInt(Txtnivel3.Text * Lblnivel310.Text)
+
+
+
+            Textnivel3.Text = Result3.ToString()
+
+        Catch
+
+
+
+        End Try
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Btnexit_Click(sender As Object, e As EventArgs) Handles Btnexit.Click
+        Try
+            MessageBox.Show("¡Gracias por usar nuestra aplicación de venta! ")
+            Application.Exit()
+
+        Catch ex As Exception
+            MessageBox.Show("Gracias por visitarnos de volleyball")
+        End Try
+    End Sub
+
+End Class
