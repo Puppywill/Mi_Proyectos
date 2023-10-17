9264501995affdd602c6471e2edcd0b205d4ae5c
William Rosado <109486989+Puppywill@users.noreply.github.com>
2023-10-17 18:40:38 -0400

    Volleyball asisten
    Visual basic 
    Volleyball with asisten

---
+++ Volleyballasisten/Volleyballasisten/Form1.Designer.vb
@@ -0,0 +1,295 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        Lblnivel310 = New Label()
+        Lblnivel215 = New Label()
+        Lblnivel120 = New Label()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Gropodeventa = New GroupBox()
+        Txtnivel3 = New TextBox()
+        Txtnivel2 = New TextBox()
+        Txtnivel1 = New TextBox()
+        Lblnivel3data = New Label()
+        Lblnivel2data = New Label()
+        Lblnivel1data = New Label()
+        Resultadogroup = New GroupBox()
+        Listboxresultado = New ListBox()
+        Lbltotalbro = New Label()
+        BtnResultado = New Button()
+        ButtonClear = New Button()
+        Btnexit = New Button()
+        GroupBox1.SuspendLayout()
+        Gropodeventa.SuspendLayout()
+        Resultadogroup.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(158, 3)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(477, 86)
+        Label1.TabIndex = 0
+        Label1.Text = "Volleyball Team"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Lblnivel310)
+        GroupBox1.Controls.Add(Lblnivel215)
+        GroupBox1.Controls.Add(Lblnivel120)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Location = New Point(12, 81)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(151, 135)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Taquillas"
+        ' 
+        ' Lblnivel310
+        ' 
+        Lblnivel310.AutoSize = True
+        Lblnivel310.Location = New Point(69, 104)
+        Lblnivel310.Name = "Lblnivel310"
+        Lblnivel310.Size = New Size(43, 15)
+        Lblnivel310.TabIndex = 6
+        Lblnivel310.Text = "$10.00 "
+        ' 
+        ' Lblnivel215
+        ' 
+        Lblnivel215.AutoSize = True
+        Lblnivel215.Location = New Point(67, 67)
+        Lblnivel215.Name = "Lblnivel215"
+        Lblnivel215.Size = New Size(43, 15)
+        Lblnivel215.TabIndex = 5
+        Lblnivel215.Text = "$15.00 "
+        ' 
+        ' Lblnivel120
+        ' 
+        Lblnivel120.AutoSize = True
+        Lblnivel120.Location = New Point(67, 34)
+        Lblnivel120.Name = "Lblnivel120"
+        Lblnivel120.Size = New Size(43, 15)
+        Lblnivel120.TabIndex = 2
+        Lblnivel120.Text = "$20.00 "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(0, 104)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(46, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "Nivel 3:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(0, 67)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(46, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Nivel 2:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(0, 34)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(46, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Nivel 1:"
+        ' 
+        ' Gropodeventa
+        ' 
+        Gropodeventa.Controls.Add(Txtnivel3)
+        Gropodeventa.Controls.Add(Txtnivel2)
+        Gropodeventa.Controls.Add(Txtnivel1)
+        Gropodeventa.Controls.Add(Lblnivel3data)
+        Gropodeventa.Controls.Add(Lblnivel2data)
+        Gropodeventa.Controls.Add(Lblnivel1data)
+        Gropodeventa.Location = New Point(12, 232)
+        Gropodeventa.Name = "Gropodeventa"
+        Gropodeventa.Size = New Size(173, 112)
+        Gropodeventa.TabIndex = 2
+        Gropodeventa.TabStop = False
+        Gropodeventa.Text = "Datos"
+        ' 
+        ' Txtnivel3
+        ' 
+        Txtnivel3.Location = New Point(67, 89)
+        Txtnivel3.Name = "Txtnivel3"
+        Txtnivel3.Size = New Size(100, 23)
+        Txtnivel3.TabIndex = 7
+        ' 
+        ' Txtnivel2
+        ' 
+        Txtnivel2.Location = New Point(67, 51)
+        Txtnivel2.Name = "Txtnivel2"
+        Txtnivel2.Size = New Size(100, 23)
+        Txtnivel2.TabIndex = 6
+        ' 
+        ' Txtnivel1
+        ' 
+        Txtnivel1.Location = New Point(67, 22)
+        Txtnivel1.Name = "Txtnivel1"
+        Txtnivel1.Size = New Size(100, 23)
+        Txtnivel1.TabIndex = 3
+        ' 
+        ' Lblnivel3data
+        ' 
+        Lblnivel3data.AutoSize = True
+        Lblnivel3data.Location = New Point(6, 92)
+        Lblnivel3data.Name = "Lblnivel3data"
+        Lblnivel3data.Size = New Size(40, 15)
+        Lblnivel3data.TabIndex = 5
+        Lblnivel3data.Text = "Nivel3"
+        ' 
+        ' Lblnivel2data
+        ' 
+        Lblnivel2data.AutoSize = True
+        Lblnivel2data.Location = New Point(6, 57)
+        Lblnivel2data.Name = "Lblnivel2data"
+        Lblnivel2data.Size = New Size(40, 15)
+        Lblnivel2data.TabIndex = 4
+        Lblnivel2data.Text = "Nivel2"
+        ' 
+        ' Lblnivel1data
+        ' 
+        Lblnivel1data.AutoSize = True
+        Lblnivel1data.Location = New Point(6, 30)
+        Lblnivel1data.Name = "Lblnivel1data"
+        Lblnivel1data.Size = New Size(40, 15)
+        Lblnivel1data.TabIndex = 3
+        Lblnivel1data.Text = "Nivel1"
+        ' 
+        ' Resultadogroup
+        ' 
+        Resultadogroup.Controls.Add(Listboxresultado)
+        Resultadogroup.Controls.Add(Lbltotalbro)
+        Resultadogroup.Location = New Point(347, 92)
+        Resultadogroup.Name = "Resultadogroup"
+        Resultadogroup.Size = New Size(411, 287)
+        Resultadogroup.TabIndex = 4
+        Resultadogroup.TabStop = False
+        Resultadogroup.Text = "Total"
+        ' 
+        ' Listboxresultado
+        ' 
+        Listboxresultado.FormattingEnabled = True
+        Listboxresultado.ItemHeight = 15
+        Listboxresultado.Location = New Point(38, 66)
+        Listboxresultado.Name = "Listboxresultado"
+        Listboxresultado.Size = New Size(336, 199)
+        Listboxresultado.TabIndex = 16
+        ' 
+        ' Lbltotalbro
+        ' 
+        Lbltotalbro.AutoSize = True
+        Lbltotalbro.Font = New Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point)
+        Lbltotalbro.Location = New Point(174, 19)
+        Lbltotalbro.Name = "Lbltotalbro"
+        Lbltotalbro.Size = New Size(57, 30)
+        Lbltotalbro.TabIndex = 15
+        Lbltotalbro.Text = "Total"
+        ' 
+        ' BtnResultado
+        ' 
+        BtnResultado.Location = New Point(225, 385)
+        BtnResultado.Name = "BtnResultado"
+        BtnResultado.Size = New Size(80, 30)
+        BtnResultado.TabIndex = 5
+        BtnResultado.Text = "Resultado "
+        BtnResultado.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonClear
+        ' 
+        ButtonClear.Location = New Point(357, 385)
+        ButtonClear.Name = "ButtonClear"
+        ButtonClear.Size = New Size(80, 30)
+        ButtonClear.TabIndex = 6
+        ButtonClear.Text = "Clear"
+        ButtonClear.UseVisualStyleBackColor = True
+        ' 
+        ' Btnexit
+        ' 
+        Btnexit.Location = New Point(480, 385)
+        Btnexit.Name = "Btnexit"
+        Btnexit.Size = New Size(80, 30)
+        Btnexit.TabIndex = 7
+        Btnexit.Text = "Exit"
+        Btnexit.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Btnexit)
+        Controls.Add(ButtonClear)
+        Controls.Add(BtnResultado)
+        Controls.Add(Resultadogroup)
+        Controls.Add(Gropodeventa)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        Gropodeventa.ResumeLayout(False)
+        Gropodeventa.PerformLayout()
+        Resultadogroup.ResumeLayout(False)
+        Resultadogroup.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Lblnivel310 As Label
+    Friend WithEvents Lblnivel215 As Label
+    Friend WithEvents Lblnivel120 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Gropodeventa As GroupBox
+    Friend WithEvents Lblnivel1data As Label
+    Friend WithEvents Txtnivel3 As TextBox
+    Friend WithEvents Txtnivel2 As TextBox
+    Friend WithEvents Txtnivel1 As TextBox
+    Friend WithEvents Lblnivel3data As Label
+    Friend WithEvents Lblnivel2data As Label
+    Friend WithEvents Resultadogroup As GroupBox
+    Friend WithEvents Lbltotalbro As Label
+    Friend WithEvents BtnResultado As Button
+    Friend WithEvents ButtonClear As Button
+    Friend WithEvents Btnexit As Button
+    Friend WithEvents Listboxresultado As ListBox
+End Class
---
+++ Volleyballasisten/Volleyballasisten/Form1.vb
@@ -0,0 +1,107 @@
+﻿
+
+'Programa de venta volleyball'
+'William A.Rosado'
+'Y00599440'
+'Comp-3900'
+
+
+Public Class Form1
+
+    Private Sub BtnResultado_Click(sender As Object, e As EventArgs) Handles BtnResultado.Click
+        Dim totalIngresos As Integer = 0
+        Dim totalnivel As Integer = 0
+        Dim totalnivel2 As Integer = 0
+        Dim totalnivel3 As Integer = 0
+        Dim totalAsistencia As Integer = 0
+
+        Dim dia As Integer = 1
+
+        ' Lista para almacenar los nombres de los días de la semana
+        Dim diasSemana As New List(Of String) From {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"}
+
+        ' Variable para almacenar el resultado final
+        Dim resultadoTexto As String = ""
+
+
+        Const cmulti As Integer = 20
+        Const cmulti2 As Integer = 15
+        Const cmulti3 As Integer = 10
+
+        Do While dia <= 7
+            Try
+
+
+                ' Obtiene la cantidad de asientos vendidos para el día actual
+                Dim nivel1 As Integer = CInt(InputBox("Ingrese la cantidad de asientos vendidos para el Nivel 1 del " & diasSemana(dia - 1)))
+                Dim nivel2 As Integer = CInt(InputBox("Ingrese la cantidad de asientos vendidos para el Nivel 2 del " & diasSemana(dia - 1)))
+                Dim nivel3 As Integer = CInt(InputBox("Ingrese la cantidad de asientos vendidos para el Nivel 3 del " & diasSemana(dia - 1)))
+
+                ' Calcula el ingreso para el día actual y lo agrega al resultadoTexto
+                Dim ingresoDia As Integer = nivel1 * cmulti + nivel2 * cmulti2 + nivel3 * cmulti3
+                Dim total1 As Integer = nivel1 * cmulti
+                Dim total2 As Integer = nivel2 * cmulti2
+                Dim total3 As Integer = nivel3 * cmulti3
+
+                Dim ingresoporDia As Integer = nivel1 + nivel2 + nivel3
+
+                Listboxresultado.Items.Add(resultadoTexto)
+                Listboxresultado.Items.Add((diasSemana(dia - 1)))
+                Listboxresultado.Items.Add("Nivel 1:" & vbCrLf & nivel1.ToString())
+                Listboxresultado.Items.Add("Nivel 2:" & vbCrLf & nivel2.ToString())
+                Listboxresultado.Items.Add("Nivel 3:" & vbCrLf & nivel3.ToString())
+
+
+                ' Suma el ingreso del día al total de ingresos
+                totalIngresos += ingresoDia
+                totalnivel += nivel1
+                totalnivel2 += nivel2
+                totalnivel3 += nivel3
+                totalAsistencia = totalnivel + totalnivel2 + totalnivel3
+            Catch ex As Exception
+                MessageBox.Show("Por favor, ingrese datos válidos para el día " & diasSemana(dia - 1))
+                Exit Sub
+            End Try
+
+            ' Incrementa el día para la próxima iteración del bucle
+            dia += 1
+        Loop
+
+        ' Muestra el ingreso total de la semana y los ingresos por día en Lblresultado1
+        Listboxresultado.Items.Add(resultadoTexto)
+        Listboxresultado.Items.Add("Ingresos por asistencia:")
+        Listboxresultado.Items.Add("Nivel 1:" & vbCrLf & totalnivel.ToString("n0"))
+        Listboxresultado.Items.Add("Nivel 2:" & vbCrLf & totalnivel2.ToString("n0"))
+        Listboxresultado.Items.Add("Nivel 3:" & vbCrLf & totalnivel3.ToString("n0"))
+        Listboxresultado.Items.Add(resultadoTexto)
+        Listboxresultado.Items.Add("Total asistencia:" & vbCrLf & totalAsistencia.ToString("n0"))
+        Listboxresultado.Items.Add(resultadoTexto)
+        Listboxresultado.Items.Add("Ingreso total de la semana: " & totalIngresos.ToString("c2"))
+        Listboxresultado.Items.Add("")
+
+    End Sub
+
+    ' Resto del código aquí...
+
+
+
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        ' Limpia los datos en los TextBox y Labels
+
+        Txtnivel1.Clear()
+        Txtnivel2.Clear()
+        Txtnivel3.Clear()
+
+        Listboxresultado.Items.Clear()
+
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles Btnexit.Click
+        ' Sale de la aplicación
+        Application.Exit()
+    End Sub
+
+
+End Class
+
